7309f732a5d0d2a44f9473827ef785f01660e813
phamdung2209 <119159181+phamdung2209@users.noreply.github.com>
2023-06-29 13:23:25 +0700

    update

--- routes/web.php
+++ routes/web.php
@@ -70,7 +70,30 @@ Route::get('manage-products', [authController::class, 'dashboard'])
     ->name('dashboard')
     ->middleware('checkAdmin');
 
+// ================================================================================================================
+// 1. Product
+
 Route::get('add-product', [addProduct_controller::class, 'index'])->name('add-Product')->middleware('checkAdmin');
+Route::post('addPro', [addProduct_controller::class, 'addPro'])->name('addPro')->middleware('checkAdmin');
+
+// eidt product
+Route::get('update-product/{id}/{name}', [addProduct_controller::class, 'updateProduct'])->name('updateProduct')->middleware('checkAdmin');
+Route::post('edit/{id}', [addProduct_controller::class, 'edit'])->name('edit')->middleware('checkAdmin');
+Route::get('delete/{id}',[addProduct_controller::class, 'delete'])->name('delete')->middleware('checkAdmin');
+
+// copy product
+// Route::get('add-product-copy/{id}', [addProduct_controller::class, 'copyPro'])->name('copyPro');
+// Route::post('add-product-copy-ct/{id}', [addProduct_controller::class, 'copyProct'])->name('copyProct');
+
+Route::get('add-product-copy/{id}/{name}', [addProduct_controller::class, 'copyPro'])->name('copyPro')->middleware('checkAdmin');
+Route::post('quickCopy', [addProduct_controller::class, 'quickCopy'])->name('quickCopy')->middleware('checkAdmin');
+
+
+
+
+// ================================================================================================================
+// 2. Categories
+Route::get('add-category', [addProduct_controller::class, 'index'])->name('add-Product')->middleware('checkAdmin');
 Route::post('addPro', [addProduct_controller::class, 'addPro'])->name('addPro')->middleware('checkAdmin');
 
 // eidt product
