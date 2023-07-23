64779f6c20dd0822b3b6f5a9cc7c84b23606d8c7
phamdung2209 <119159181+phamdung2209@users.noreply.github.com>
2023-07-23 17:47:40 +0700

    add cart function

--- app/Models/cartModel.php
+++ app/Models/cartModel.php
@@ -12,13 +12,14 @@ class cartModel extends Model
     protected $primaryKey = "id";
     protected $fillable = ['quantity', 'id_user'];
     public $timestamps = true;
-    public function cart_pro(){
+    public function cart_pro()
+    {
         return $this->hasMany(cart_proModel::class, "id_cart", "id");
     }
     public function account()
-{
-    return $this->belongsTo(Account::class, 'id_user');
-}
+    {
+        return $this->belongsTo(Account::class, 'id_user');
+    }
 
 
 }
