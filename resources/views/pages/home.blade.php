7d3285bf6e9b3f01afd03553fbda172e161d24e6
phamdung2209 <phamdung.22092003@gmail.com>
2023-07-29 15:55:15 +0700

    responsive

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -38,7 +38,7 @@
                                                         <span class="sr-only">Next</span>
                                                     </a>
                                                 </div> -->
-                    <img src="{{ asset('images/banner-slide.png') }}" alt="" style="height: 348px">
+                    <img src="{{ asset('images/banner-slide1.png') }}" alt="">
                     {{-- <img src="{{ asset('images/banner-Slide1.png') }}" alt=""> --}}
                     <!-- end -->
 
